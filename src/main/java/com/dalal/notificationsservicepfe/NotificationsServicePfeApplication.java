cdf6717f16384ba8ea7d0061fa734a455f142cd4
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-08-17 21:24:33 +0100

    create project

---
+++ src/main/java/com/dalal/notificationsservicepfe/NotificationsServicePfeApplication.java
@@ -0,0 +1,13 @@
+package com.dalal.notificationsservicepfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsServicePfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationsServicePfeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dalal/notificationsservicepfe/NotificationsServicePfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalal.notificationsservicepfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsServicePfeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
