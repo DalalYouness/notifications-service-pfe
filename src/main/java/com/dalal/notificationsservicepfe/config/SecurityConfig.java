e0cba1b73c3cc0697bf96ec39eaefaff06e03bb7
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-08-18 12:56:38 +0100

    security(config): configure SecurityFilterChain

--- src/main/java/com/dalal/notificationsservicepfe/config/SecurityConfig.java
+++ src/main/java/com/dalal/notificationsservicepfe/config/SecurityConfig.java
@@ -1,4 +1,50 @@
 package com.dalal.notificationsservicepfe.config;
 
+import com.dalal.notificationsservicepfe.security.CustomAccessDeniedHandler;
+import com.dalal.notificationsservicepfe.security.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+     private final CustomAuthenticationEntryPoint  customAuthenticationEntryPoint;
+     private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    //private final JwtAuthenticationFilter  jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain  securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        // 1. (Public / Documentation)
+                        // you see every end points in request http matchers that endpoints permit All
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/actuator/health"
+                        ).permitAll()
+                        // any other request have to have authenticated
+                        // we handle that authorisation either in that security config but preferable to use it inside controllers
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                        .accessDeniedHandler(customAccessDeniedHandler));
+                //.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
